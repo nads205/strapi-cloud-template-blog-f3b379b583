4bfd208e84951694064d02400f64eacf0d824844
nads205 <nads205@users.noreply.github.com>
2025-07-02 17:31:37 +0100

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,23 +1,12 @@
 module.exports = [
-  'strapi::errors',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      header: '*',
-      origin: [
-        'https://students.plaincc.com',
-        'http://localhost:3000', // for local development
-        'https://blpdepuo.gensparkspace.com' // for testing
-      ]
-    }
-  },
-  'strapi::poweredBy',
   'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-  'strapi::security',
 ];
