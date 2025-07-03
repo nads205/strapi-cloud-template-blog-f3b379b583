303e6ff8ee95da7545ad506564dd323a3a64e33d
Naadir Akhtar <naadirakhtar@hotmail.com>
2025-07-03 16:53:46 +0100

    adding staging

--- config/middlewares.js
+++ config/middlewares.js
@@ -10,7 +10,8 @@ module.exports = [
         'https://blpdepuo.gensparkspace.com',
         'https://npoaosyd.gensparkspace.com',
         'https://slxvhrfi.gensparkspace.com',
-        'https://jolly-meadow-065c3c003-4.westeurope.6.azurestaticapps.net'
+        'https://jolly-meadow-065c3c003-4.westeurope.6.azurestaticapps.net',
+        'https://jolly-meadow-065c3c003-5.westeurope.6.azurestaticapps.net'
       ]
     }
   },
