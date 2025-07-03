4dd697295f8ca0f9069dfa3cee9db6b600e9abee
nads205 <nads205@users.noreply.github.com>
2025-07-03 14:52:04 +0100

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -9,7 +9,8 @@ module.exports = [
         'http://localhost:3000',
         'https://blpdepuo.gensparkspace.com',
         'https://npoaosyd.gensparkspace.com',
-        'https://slxvhrfi.gensparkspace.com'
+        'https://slxvhrfi.gensparkspace.com',
+        'https://jolly-meadow-065c3c003-4.westeurope.6.azurestaticapps.net'
       ]
     }
   },
